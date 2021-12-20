7894dc542ca3a24ac06224c6d02b9d14d76e016d
ivan <volovyk@soley.io>
2021-12-20 22:40:41 +0200

    bf

--- Registry/assembly/__tests__/guestbook.spec.ts
+++ Registry/assembly/__tests__/guestbook.spec.ts
@@ -1,9 +1,9 @@
-import { addGameAddress, getGameAddresses } from "../main";
+import { addGameAddress, getGameAddresses, getGameAddress } from "../main";
 import { GameAddress, gameAddresses } from "../model";
 import { VMContext, Context, u128 } from "near-sdk-as";
 
-function createGameAddress(text: string): GameAddress {
-  return new GameAddress(text);
+function createGameAddress(contractAddress: string): GameAddress {
+  return new GameAddress(contractAddress);
 }
 
 const gameAddress = createGameAddress("hello world");
@@ -24,13 +24,6 @@ describe("gameAddress tests", () => {
     );
   });
 
-  it("adds a premium gameAddress", () => {
-    VMContext.setAttached_deposit(u128.from("10000000000000000000000"));
-    addGameAddress("hello world");
-    const gameAddressAR = getGameAddresses();
-    expect(gameAddressAR[0].premium).toStrictEqual(true, "should be premium");
-  });
-
   it("retrieves gameAddresses", () => {
     addGameAddress("hello world");
     const gameAddressesArr = getGameAddresses();
@@ -41,45 +34,32 @@ describe("gameAddress tests", () => {
     );
   });
 
-  it("only show the last 10 gameAddresses", () => {
+  it("retrieves gameAddress by id", () => {
     addGameAddress("hello world");
-    const newMessages: GameAddress[] = [];
-    for (let i: i32 = 0; i < 10; i++) {
+    const _gameAddresses = getGameAddress(0);
+    expect(_gameAddresses).toIncludeEqual(
+      gameAddress,
+      "gameAddresses should include:\n" + gameAddress.toJSON()
+    );
+  });
+
+  it("only show the last 20 gameAddresses", () => {
+    addGameAddress("hello world");
+    const newAddresses: GameAddress[] = [];
+    for (let i: i32 = 0; i < 20; i++) {
       const text = "gameAddress #" + i.toString();
-      newMessages.push(createGameAddress(text));
+      newAddresses.push(createGameAddress(text));
       addGameAddress(text);
     }
     const gameAddresses = getGameAddresses();
     log(gameAddresses.slice(7, 10));
     expect(gameAddresses).toStrictEqual(
-      newMessages,
+      newAddresses,
       "should be the last ten gameAddresses"
     );
     expect(gameAddresses).not.toIncludeEqual(
       gameAddress,
       "shouldn't contain the first element"
-    );
-  });
-});
-
-describe("attached deposit tests", () => {
-  beforeEach(() => {
-    VMContext.setAttached_deposit(u128.fromString("0"));
-    VMContext.setAccount_balance(u128.fromString("0"));
-  });
-
-  it("attaches a deposit to a contract call", () => {
-    log("Initial account balance: " + Context.accountBalance.toString());
-
-    addGameAddress("hello world");
-    VMContext.setAttached_deposit(u128.from("10"));
-
-    log("Attached deposit: 10");
-    log("Account balance after deposit: " + Context.accountBalance.toString());
-
-    expect(Context.accountBalance.toString()).toStrictEqual(
-      "10",
-      "balance should be 10"
     );
   });
 });
--- Registry/assembly/model.ts
+++ Registry/assembly/model.ts
@@ -1,15 +1,12 @@
-import { context, u128, PersistentVector } from "near-sdk-as";
+import { context, PersistentVector } from "near-sdk-as";
 
 /**
  * Exporting a new class GameAddress so it can be used outside of this file.
  */
 @nearBindgen
 export class GameAddress {
-  premium: boolean;
   sender: string;
   constructor(public contractAddress: string) {
-    this.premium =
-      context.attachedDeposit >= u128.from("10000000000000000000000");
     this.sender = context.sender;
   }
 }
