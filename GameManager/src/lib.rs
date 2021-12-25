8ab7b273cd74e942a56f147bd45d8432f1060861
ivan <volovyk@soley.io>
2021-12-25 20:58:54 +0200

    working and not breaking

--- GameManager/src/lib.rs
+++ GameManager/src/lib.rs
@@ -1,21 +1,13 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId, Balance, Promise, PanicOnDefault, Gas};
-use near_sdk::serde::{Serialize};
-use near_sdk::serde_json;
+use near_sdk::{env, near_bindgen, AccountId, Balance, Promise};
 
 near_sdk::setup_alloc!();
 
-const INITIAL_INGAME_BALANCE: Balance = 3_200_000_000_000_000_000_000_000; // 3e24yN, 3.2N
-const GAS: Gas = 50_000_000_000_000;
-                  2_428_021_617_030
+const MIN_ATTACHED_BALANCE: Balance = 0;
+
+// const NFT_GAS_NEW: GAS = 4_201_706_103_318_740_100_000_000;
 
 const NFT_WASM_CODE: &[u8] = include_bytes!("../../SimpleNFT/res/non_fungible_token.wasm");
-
-#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault, Serialize)]
-#[serde(crate = "near_sdk::serde")]
-pub struct TokenArgs {
-    pub owner_id: AccountId,
-}
 
 // add the following attributes to prepare your code for serialization and invocation on the blockchain
 // More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
@@ -35,6 +27,12 @@ impl GameManager {
 
 
 fn create_ingame_contract(prefix: AccountId, code: Vec<u8>) -> Promise {
+
+    assert!(
+      is_valid_symbol(&prefix),
+      "Prefix is invalid"
+    );
+
     let subaccount_id = 
       format!("{}.{}", prefix, env::current_account_id());
     assert!(
@@ -42,14 +40,25 @@ fn create_ingame_contract(prefix: AccountId, code: Vec<u8>) -> Promise {
       "Token Account ID is invalid"
     );
 
-    let args: TokenArgs = TokenArgs {
-      owner_id: env::predecessor_account_id(),
-    };
+    assert!(
+      env::attached_deposit() >= MIN_ATTACHED_BALANCE,
+      "Not enough attached deposit"
+    );
 
     Promise::new(subaccount_id)
         .create_account()
+        .transfer(env::attached_deposit())
         .add_full_access_key(env::signer_account_pk())
-        .transfer(INITIAL_INGAME_BALANCE)
         .deploy_contract(code)
-        .function_call(b"new_default_meta".to_vec(), serde_json::to_vec(&args).unwrap(), 0, GAS)
+}
+
+
+fn is_valid_symbol(token_id: &str) -> bool {
+  for c in token_id.as_bytes() {
+      match c {
+          b'0'..=b'9' | b'a'..=b'z' | b'_' | b'-' => (),
+          _ => return false,
+      }
+  }
+  true
 }
