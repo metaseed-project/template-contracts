d97096f4c3ec5918cbf7c6d7e1364603cb3160e5
ivan <volovyk@soley.io>
2021-12-27 15:10:13 +0200

    add HashMap tp the Registry

--- GameManager/Manual Testing.md
+++ GameManager/Manual Testing.md
@@ -6,10 +6,16 @@ Creator=phoneiostest.testnet
 
 Receiver=ivikkktest.testnet
 
-near call $ContractID create_ingame_nft '{"prefix": "nft2"}' --accountId $Creator --depositYocto 5189980000000000000000000 --gas 300000000000000
+near call $ContractID create_ingame_nft '{"prefix": "nft3"}' --accountId $Creator --depositYocto 5189980000000000000000000 --gas 300000000000000
 
-near call nft2.$ContractID nft_mint '{"token_id": "0", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $ContractID --deposit 0.1
+near call $ContractID get_asset '{"account_id": "nft3.$ContractID"}' --accountId $Creator
 
-near call nft2.$ContractID nft_mint '{"token_id": "1", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $Creator --deposit 0.1
+## will work
 
-near call nft2.$ContractID nft_mint '{"token_id": "1", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $Receiver --deposit 0.1
+near call nft3.$ContractID nft_mint '{"token_id": "1", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $Creator --deposit 0.1
+
+## won't
+
+near call nft3.$ContractID nft_mint '{"token_id": "0", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $ContractID --deposit 0.1
+
+near call nft3.$ContractID nft_mint '{"token_id": "1", "receiver_id": "'$Receiver'", "token_metadata": { "title": "t", "description": "d", "media": "m", "copies": 1}}' --accountId $Receiver --deposit 0.1
--- GameManager/src/lib.rs
+++ GameManager/src/lib.rs
@@ -1,4 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
 use near_sdk::{env, near_bindgen, AccountId, Balance, Promise, PanicOnDefault, Gas};
 use near_sdk::serde::{Serialize};
 use near_sdk::serde_json;
@@ -18,37 +19,82 @@ pub struct TokenArgs {
     pub owner_id: AccountId,
 }
 
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct AssetOptions {
+  extra: String,
+}
+
 // add the following attributes to prepare your code for serialization and invocation on the blockchain
 // More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct GameManager {
+    pub owner_id: AccountId,
+    pub ingame_assets: LookupMap<String, AssetOptions>,
 }
 
 #[near_bindgen]
 impl GameManager {
 
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        assert!(env::state_read::<Self>().is_none(), "Already initialized");
+        Self {
+          owner_id,
+          ingame_assets: LookupMap::new(b"r".to_vec()),
+        }
+    }
+
     #[payable]
-    pub fn create_ingame_nft(prefix: AccountId) {
-        create_ingame_contract(prefix, NFT_WASM_CODE.to_vec());
+    pub fn create_ingame_nft(&mut self, prefix: AccountId) {
+
+        let subaccount_id = create_account_subaccount(prefix);
+
+        let options: AssetOptions = AssetOptions {
+          extra: "".to_string(),
+        };
+
+        self.ingame_assets.insert(&subaccount_id, &options);
+        create_ingame_contract(subaccount_id, NFT_WASM_CODE.to_vec());
+    }
+
+    pub fn get_asset(&self, account_id: AccountId) -> Option<AssetOptions> {
+      return self.ingame_assets.get(&account_id);
+    }
+
+    #[payable]
+    pub fn set_asset(&mut self, account_id: AccountId, extra: String) {
+      assert!(
+        env::is_valid_account_id(account_id.as_bytes()),
+        "Token Account ID is invalid"
+      );
+
+      let options: AssetOptions = AssetOptions {
+        extra: extra,
+      };
+
+      self.ingame_assets.insert(&account_id, &options);
     }
 }
 
+fn create_account_subaccount(prefix: AccountId) -> String {
+  assert!(
+    is_valid_symbol(&prefix),
+    "Prefix is invalid"
+  );
 
-fn create_ingame_contract(prefix: AccountId, code: Vec<u8>) -> Promise {
+  let subaccount_id = 
+    format!("{}.{}", prefix, env::current_account_id());
+  assert!(
+    env::is_valid_account_id(subaccount_id.as_bytes()),
+    "Token Account ID is invalid"
+  );
 
-    assert!(
-      is_valid_symbol(&prefix),
-      "Prefix is invalid"
-    );
+  subaccount_id
+}
 
-    let subaccount_id = 
-      format!("{}.{}", prefix, env::current_account_id());
-    assert!(
-      env::is_valid_account_id(subaccount_id.as_bytes()),
-      "Token Account ID is invalid"
-    );
-
+fn create_ingame_contract(subaccount_id: AccountId, code: Vec<u8>) -> Promise {
     assert!(
       env::attached_deposit() >= MIN_ATTACHED_BALANCE,
       "Not enough attached deposit"
